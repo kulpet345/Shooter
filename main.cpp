b781114a78d95e463a141840bbe898910c2b0af7
Yury Semenov <yusemru@gmail.com>
2021-05-01 11:47:49 +0300

    added movement

--- main.cpp
+++ main.cpp
@@ -9,27 +9,9 @@ GLFWwindow* window;
 #include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
+#include "camera.h"
 #include "enemy.h"
 #include "text2D.hpp"
-
-inline glm::mat4 get_matrix(double time){
-    // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-    // Or, for an ortho camera :
-    //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
-    
-    // Camera matrix
-    glm::mat4 View       = glm::lookAt(
-                                glm::vec3(3 * cos(time), 0, -1.5 + 3 * sin(time)),
-                                glm::vec3(0,0, -1.5),
-                                glm::vec3(0,1,0)
-                           );
-    // Model matrix : an identity matrix (model will be at the origin)
-    glm::mat4 Model      = glm::mat4(1.0f);
-    // Our ModelViewProjection : multiplication of our 3 matrices
-    glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
-    return MVP;
-}
 
 int main(int argc, char ** argv)
 {
@@ -84,10 +66,11 @@ int main(int argc, char ** argv)
     EnemyBuffer::create_enemy();
 
     do{
+        recalcCamera(window);
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        auto MVP = get_matrix(glfwGetTime());
+        auto MVP = getMVP();
 
         EnemyBuffer::try_create();
 
